f9d3aa63b5b34e29d50924632da42180f1fa9392
kolitt <ttil.neok@gmail.com>
2014-09-17 16:11:05 -0600

    Update hello.java

--- workspace/Lab4/src/hello.java
+++ workspace/Lab4/src/hello.java
@@ -3,6 +3,6 @@
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display the string
-		System.out.println("How are you doing?");
+		System.out.println("How you doing?");
 	}
 }
